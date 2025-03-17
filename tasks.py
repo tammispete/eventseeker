805ddbee136aaf376e0f62992c34a94b3138dae3
tammispete <tamminen.petteri@gmail.com>
2025-03-17 19:46:42 +0200

    added a bit more code to tikettifi

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from robocorp.tasks import task
+from robocorp import browser
 
 @task
 def eventseekerMain():
@@ -8,4 +9,22 @@ def eventseekerMain():
 
 def getgigs_from_tikettiFI():
     """go to tiketti.fi, filter dates, screenshot, send email"""
+    """slowed browser down to see whats happening"""
+    browser.configure(
+        slowmo=1500,
+    )
+    """open tiketti.fi"""
+    browser.goto("https://www.tiketti.fi/")
+    """get browserfuctions for this page"""
+    page = browser.page()
+    """get rid of cookie consent, waits for popup to appead and then clicks on the decline button"""
+    page.wait_for_selector("#CybotCookiebotDialogBodyButtonDecline", state="visible", timeout=10000)  # Waits up to 10s
+    page.click("#CybotCookiebotDialogBodyButtonDecline")
+    """click on haku"""
+    page.click("span:text('Haku')")
+    """click on calendar next month"""
+    page.click(".fa.fa-angle-right")
+    """click on calendar 1 day"""
+   
+
 
